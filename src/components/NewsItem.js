52279b834f215b54927699454b734c4bcfed7c7f
Naima Hassan <naima.hassan434@gmail.com>
2021-11-14 12:27:23 +0000

    Understanding state in class based components

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,20 +2,16 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    let { title, description } = this.props;
+    let { title, description, imgUrl, newsUrl } = this.props;
     return (
-      <div>
-        <div className="card my-3" style={{}}>
-          <img
-            src="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg"
-            className="card-img-top"
-            alt="..."
-          />
+      <div className="my-3">
+        <div className="card" style={{}}>
+          <img src={imgUrl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a href="/" className="btn btn-primary">
-              Go somewhere
+            <a href="/newsdetail/" className="btn btn-sm btn-primary">
+              Read More
             </a>
           </div>
         </div>
